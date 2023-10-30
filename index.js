f6eb90a595586a4ece647e9cea6ed5921346c064
honguyen00 <lehonguyen00@gmail.com>
2023-10-30 14:22:55 +1100

    adding comments for all file

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const inquirer = require("inquirer");
-var validateColor = require("validate-color").default;
+
+// npm package for validating color input
+const validateColor = require("validate-color").default;
+
+// fs 
 const { join } = require('path');
 const { writeFile } = require('fs/promises');
+
+// import all the shape classes
 const Triangle = require('./lib/triangle');
 const Circle = require('./lib/circle');
 const Square = require('./lib/square');
+// function to check if input is empty or not
+const isEmpty = (input) => {
+    if (input == '' || !input) {
+        return false;
+    }
+    return true;
+}
 
+// function to check for logo input
 const logoString = (input) => {
     if (isEmpty(input) && input.trim().length <= 3) {
         return true;
@@ -15,6 +29,7 @@ const logoString = (input) => {
     }
 }
 
+// function to check if input is a valid css color or not
 function isColor(input) {
     let test1 = validateColor(input.toLowerCase());
     let Reg_Exp = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i;
@@ -24,6 +39,7 @@ function isColor(input) {
     } else return false;
 }
 
+// function to check for color input
 const colorString = (input) => {
     if (isEmpty(input) && isColor(input)) {
         return true;
@@ -32,7 +48,7 @@ const colorString = (input) => {
     }
 }
 
-
+// prompt users with questions
 inquirer
     .prompt([
         {
@@ -62,31 +78,29 @@ inquirer
     ])
     .then(({ logo, textcolor, shape, backgroundcolor }) => {
         let newLogo;
+        // check if users want circle triangle or square
         switch(shape) {
             case 'Circle': newLogo = new Circle(); break;
             case 'Triangle': newLogo = new Triangle(); break;
             case 'Square': newLogo = new Square(); break;
             default: break;
         }
+        // set logo text and color
         newLogo.setText(logo);
         newLogo.setBackgroundColor(backgroundcolor.toLowerCase());
         newLogo.setTextColor(textcolor.toLowerCase());
+        // export file to output folder
         return writeFile(
             join(__dirname, '.', 'output', 'Logo.svg'),
             createSvg(newLogo.render(), newLogo.renderText())
           );
     })
 
+// function to wrap the logo in svg tag
 function createSvg(shape, text) {
     let svg = (`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
     ${shape}
     ${text}
 </svg>`)
     return svg;
-}
-const isEmpty = (input) => {
-    if (input == '' || !input) {
-        return false;
-    }
-    return true;
 }
